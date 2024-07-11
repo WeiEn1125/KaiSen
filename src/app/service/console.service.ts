4d2de279902bf434af1b4906836c3e1f86e020cc
Wei <wei_c@moonisland.jp>
2024-07-11 12:02:57 +0900

    [add] Create console service

---
+++ src/app/service/console.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+@Injectable({
+  providedIn: 'root'
+})
+export class ConsoleService {
+
+  constructor() { }
+
+  private debugMode: boolean = true;
+
+  log(...data: any[]): void {
+    if (this.debugMode) {
+      console.log(...data);
+    }
+  }
+
+  info(...data: any[]): void {
+    if (this.debugMode) {
+      console.info(...data);
+    }
+  }
+
+  warn(...data: any[]): void {
+    if (this.debugMode) {
+      console.warn(...data);
+    }
+  }
+
+  error(...data: any[]): void {
+    console.error(...data);
+  }
+
+  debug(...data: any[]): void {
+    if (this.debugMode) {
+      console.debug(...data);
+    }
+  }
+
+  assert(expression: boolean, ...data: any[]): void {
+    if (this.debugMode) {
+      console.assert(expression, ...data);
+    }
+  }
+
+  table(data: any, properties?: string[]): void {
+    if (this.debugMode) {
+      console.table(data, properties);
+    }
+  }
+
+}
